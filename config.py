b176aa0ff054fbe5f34a2ca26606f4ce3ef2d386
Antoh1 <antotiro@gmail.com>
2016-11-23 01:56:41 +0300

    added app configurations

---
+++ config.py
@@ -0,0 +1,24 @@
+
+import os
+
+# Enabling the development environment
+DEBUG = True
+
+# Define the application directory
+basedir = os.path.abspath(os.path.dirname(__file__))  
+
+SQLALCHEMY_TRACK_MODIFICATIONS = True
+
+# Define the database - working with SQLite 
+SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db')
+SQLALCHEMY_MIGRATE_REPO = os.path.join(basedir, 'db_repository')
+
+# Enable protection agains *Cross-site Request Forgery (CSRF)*
+CSRF_ENABLED     = True
+
+# Use a secure, unique and absolutely secret key for
+# signing the data. 
+CSRF_SESSION_KEY = "willyouevereguessthis"
+
+# Secret key for signing cookies
+SECRET_KEY = "nowayyoucanguess"
