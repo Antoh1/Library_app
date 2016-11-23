29656c141a10dbffdcf8384b3896d5de54c6125f
Antoh1 <antotiro@gmail.com>
2016-11-23 12:54:26 +0300

    added admin login controller/route function

--- app/views.py
+++ app/views.py
@@ -6,12 +6,19 @@ from werkzeug import check_password_hash, generate_password_hash
 # Import the database object from the main app module
 from app import app, db
 # Import module forms
-from forms import signupForm
-from forms import loginForm
+from forms import signupForm, loginForm
+
+from flask_login import login_user 
+
+from flask_login import login_required
 # Import module models 
 from models import User
 
 # Set the route and accepted methods
+@app.route('/')
+@app.route('/Home')
+def Home():
+    return render_template('Home.html', title='Home')
 @app.route('/signup', methods=['GET', 'POST'])
 def signup():
 
@@ -25,30 +32,30 @@ def signup():
         db.session.add(user)
         db.session.commit()
         flash('You are now a Library valid User')
-        return render_template('login.html', form=form)
+        return render_template('user_login.html', form=form)
     else:
         return render_template('signup.html', form=form)
 # decorator to register route for user login controller
 @app.route('/user_login', methods=['GET','POST'])
 def user_login():
     error = None
-    form = loginForm(request.form)
+    user_form = loginForm(request.form)
     if request.method == 'POST':
-        if form.validate_on_submit():
-            #if request.form['email'] != 'admin@gmail.com' or request.form['password'] != 'admin':
-                #error = 'Invalid login credentials, Try again'
-            else:
+        if user_form.validate_on_submit():
+            user = User.query.filter_by(email=user_form.email.data).first()
+            if user is not None and user.verify_password(user_form.password.data):
+                login_user(user)
                 session['logged_in']=True
                 flash('You are logged in')
-                return redirect(url_for('signup'))
-        else:
-            render_template('user_login.html', form=form, error=error)
-    return render_template('user_login.html', form=form, error=error) 
+                return redirect(url_for('Home'))
+            flash('Invalid username or password.')    
+        return render_template('user_login.html', form=user_form, error=error)
+    return render_template('user_login.html', form=user_form, error=error) 
 
 #decorator to register route for admin login controller
 @app.route('/admin_login', methods=['GET','POST'])
 def admin_login():
-     error = None
+    error = None
     form = loginForm(request.form)
     if request.method == 'POST':
         if form.validate_on_submit():
@@ -57,7 +64,7 @@ def admin_login():
             else:
                 session['logged_in']=True
                 flash('You are logged in')
-                return redirect(url_for('admin_panel'))
+                return redirect(url_for('Home'))
         else:
             render_template('admin_login.html', form=form, error=error)
     return render_template('admin_login.html', form=form, error=error)                         
