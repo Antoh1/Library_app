0db737592f8fdfa8c1d90211f9609bc1b363fe18
Antoh1 <antotiro@gmail.com>
2016-11-25 08:33:02 +0300

    deploy to heroku

--- app/forms.py
+++ app/forms.py
@@ -1,24 +1,50 @@
 
 #Import Form elements
 from flask_wtf import Form
-from wtforms import StringField, PasswordField, BooleanField, TextField, SubmitField, IntegerField
+#import form field variable types
+from wtforms import StringField, PasswordField, BooleanField, TextField, SubmitField, IntegerField, ValidationError
 # Import Form validators
-from wtforms.validators import DataRequired, Email, Length 
+from wtforms.validators import DataRequired, Email, Length, Regexp, EqualTo 
+from .models import User
 
 # Define the signup form (WTForms)
-class signupForm(Form):    	
-	name = StringField('Last Name', validators=[DataRequired()])
-	email    = TextField('Email Address', validators=[Email(message='Put the right mail address'), DataRequired(message='Forgot your email address?')])
-	id_no = StringField('ID Number', validators=[DataRequired()])
-	password = PasswordField('Password', validators=[DataRequired(message="Must provide password")])
-	submit = SubmitField('SignUp')
+class SignupForm(Form):
+    """This class instanciate signup form 
+    instance to be rendered to SignUp html template"""
 
-class loginForm(Form):
+    name = StringField('Last Name', validators=[DataRequired(), 
+        Regexp('^[A-Za-z][A-Za-z0-9_.]*$', 0,'Usernames must have only letters, numbers, dots or underscores')])
+    email    = TextField('Email Address', 
+        validators=[Email(message='Put the right mail address'), DataRequired(message='Forgot your email address?'), Length(1, 64)])
+    id_no = StringField('ID Number', validators=[DataRequired()])
+    password = PasswordField('Password', validators=[DataRequired(), EqualTo('password2', message='Passwords must match.')])
+    password2 = PasswordField('Confirm password', validators=[DataRequired()])
+
+    def validate_email(self, field):
+        if User.query.filter_by(email=field.data).first():
+            raise ValidationError('Email already registered.')
+
+    def validate_id_no(self, field):
+        if User.query.filter_by(email=field.data).first():
+            raise ValidationError('ID Number already registered.')
+
+    def validate_name(self, field):
+        if User.query.filter_by(name=field.data).first():
+            raise ValidationError('Name already in use.') 
+
+
+class LoginForm(Form):
+    """This class instanciate login form 
+    instance to be rendered to login html template"""
+
     email = TextField('Email Address', validators=[Email(), DataRequired()])
     password = PasswordField('Password', validators=[DataRequired()])
 
-class addBook(Form):
+
+class AddBook(Form):
+    """This class instanciate add_Book form 
+    instance to be rendered to admin_dashboard html template"""
+
     book_name = TextField('Book Name', validators=[DataRequired()])
     book_desc = TextField('Book Description', validators=[DataRequired()])
-    book_category = TextField('Book Category', validators=[DataRequired()])
-    submit = SubmitField('ADD')    
+    book_category = TextField('Book Category', validators=[DataRequired()])    
--- app/models.py
+++ app/models.py
@@ -1,15 +1,17 @@
 # Import the database object (db) from the main application module
 from app import db
-
+#import security module for encrypting passwords in the database
 from werkzeug.security import generate_password_hash, check_password_hash
-
+#import UserMixin module for validating user login states
 from flask_login import UserMixin
-
+#login manager to validate user login
 from . import loginManager
 
 # Define a User model class as one of the database tables
 
-class User(UserMixin, db.Model):
+class User(db.Model, UserMixin):
+    """This class creates users database table instance
+     and sets table field names"""
 
     __tablename__ = 'users'
 
@@ -19,31 +21,35 @@ class User(UserMixin, db.Model):
     name = db.Column(db.String,  nullable=False)
 
     # Identification Data: email , id_no and password 
-    email    = db.Column(db.String,  index=True,
-                                            unique=True)
-    id_no    = db.Column(db.Integer,  nullable=False,
-                                            unique=True)
-    password_hash = db.Column(db.String,  nullable=False)
+    email    = db.Column(db.String,  index=True, unique=True)
 
+    id_no    = db.Column(db.Integer, nullable=False, unique=True)
+
+    password_hash = db.Column(db.String, nullable=True)
+    
     def __init__(self, name, email, id_no, password):
         self.name = name
         self.email = email
         self.id_no = id_no
-        self.password_hash = password
-
+        self.password_set(password)
+   
+    #property checking for hashable password input
     @property   
     def password(self):        
         raise AttributeError('password is not a readable attribute')
         
-    @password.setter    
-    def password(self, password):   
+        
+    def password_set(self, password):
+        #method to encrypt password stored in database   
         self.password_hash = generate_password_hash(password)
 
     def verify_password(self, password):
+        #method to compare encrypted password in database and post request
         return check_password_hash(self.password_hash, password) 
 
     @loginManager.user_loader 
-    def load_user(user_id):    
+    def load_user(user_id):
+        #method to validate user login    
         return User.query.get(int(user_id))     
 
 
@@ -51,8 +57,10 @@ class User(UserMixin, db.Model):
     def __repr__(self):
         return '<User %r>' % (self.name) 
 
-class availableBooks(db.Model):
-    
+class AvailableBooks(db.Model):
+    """This class creates available_books database table instance
+     and sets table field names"""
+
     __tablename__ = 'available_books'
 
     id = db.Column(db.Integer, primary_key=True)
@@ -60,8 +68,6 @@ class availableBooks(db.Model):
     book_name = db.Column(db.String, nullable=False)
 
     book_desc = db.Column(db.Text, nullable=False)
-
-    book_quantity = db.Column(db.Numeric, nullable=False)
 
     book_category = db.Column(db.Text, nullable=False)
 
@@ -77,7 +83,9 @@ class availableBooks(db.Model):
         return '<Book %r>' % (self.book_name) 
 
 
-class borrowedBooks(db.Model):
+class BorrowedBooks(db.Model):
+    """This class creates borrowed_books database table instance
+     and sets table field names"""
 
     __tablename__ = 'borrowed_books'
 
@@ -102,7 +110,28 @@ class borrowedBooks(db.Model):
         self.delay_charge = delayCharge
 
     def __repr__(self):
+        return '<Book %r>' % (self.book_name)
+
+class TotalBooks(db.Model):
+    """This class creates total books database instance table object"""
+
+    __tablename__ = 'total_books'
+
+    id = db.Column(db.Integer, primary_key=True)
+
+    book_name = db.Column(db.String, nullable=False, index=True)
+
+    book_quantity = db.Column(db.Integer, nullable=False)
+
+    def __init__(self, bookName, bookQuantity):
+
+        self.book_name = bookName
+        self.book_quantity = bookQuantity
+
+    def __repr__(self):
         return '<Book %r>' % (self.book_name)    
+
+
 
 
 
