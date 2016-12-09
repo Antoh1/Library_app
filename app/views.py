09f0f9a6a6ab4d59be12118c48c36e6eda36abda
Antoh1 <antotiro@gmail.com>
2016-12-09 21:09:54 +0300

    Updates user signup and login views to intergrate flask bootstrap

--- app/views.py
+++ app/views.py
@@ -8,9 +8,9 @@ from app import app, db
 # Import module forms
 from forms import SignupForm, LoginForm, AddBook
 
-from flask_login import login_user, logout_user, login_required 
+from flask_login import login_user, logout_user, login_required
 
-# Import module models 
+# Import module models
 from models import User, AvailableBooks
 
 
@@ -20,12 +20,12 @@ from models import User, AvailableBooks
 def Home():
     """This method renders the landing page upon GET reques by user"""
 
-    return render_template('Home.html', title='Home')
+    return render_template('Home.html')
 
 
 @app.route('/signup', methods=['GET', 'POST'])
 def signup():
-    """This function renders signup form to signup html template 
+    """This function renders signup form to signup html template
     and handles the POST request from the form"""
 
 	# If sign in form is submitted
@@ -33,20 +33,20 @@ def signup():
 
     # Verify the signup in form
     if form.validate_on_submit():
-        user = User(name= form.name.data, email=form.email.data, 
+        user = User(name= form.name.data, email=form.email.data,
             id_no=form.id_no.data, password=form.password.data)
         db.session.add(user)
         db.session.commit()
         flash('You are now a Library valid User')
         return redirect(url_for('user_login'))
-    flash('Enter unique Name, Email and ID Number')    
+    flash('Enter unique Name, Email and ID Number')
     return render_template('signup.html', form=form)
 
 
 # decorator to register route for user login controller
 @app.route('/user_login', methods=['GET','POST'])
 def user_login():
-    """This function renders login form to login html template 
+    """This function renders login form to login html template
     and handles the POST request from the form"""
 
     error = None
@@ -58,15 +58,14 @@ def user_login():
             session['logged_in']=True
             flash('You are logged in as Library User')
             return redirect(url_for('.viewBooks'))
-        flash('Invalid username or password.')    
+        flash('Invalid username or password.')
         return render_template('user_login.html', form=user_form, error=error)
-    flash('Enter a valid email address')    
-    return render_template('user_login.html', form=user_form, error=error) 
+    return render_template('user_login.html', form=user_form, error=error)
 
 #decorator to register route for admin login controller
 @app.route('/admin_login', methods=['GET','POST'])
 def admin_login():
-    """This function renders administrator login form to admin login html template 
+    """This function renders administrator login form to admin login html template
     and handles the POST request from the form"""
 
     error = None
@@ -85,15 +84,16 @@ def admin_login():
     return render_template('admin_login.html', form=admin_form, error=error)
 
 @app.route('/logout')
-@login_required 
+@login_required
 def logout():
     logout_user()
     flash('You have been logged out.')
-    return redirect(url_for('Home')) 
+    return redirect(url_for('Home'))
 
 @app.route('/admin_dashboard', methods=['GET','POST'])
+@login_required
 def add_Book():
-    """This function renders addBook form to admin_dashboard html template 
+    """This function renders addBook form to admin_dashboard html template
     and updates the POST request from the form to the database table"""
 
     form = AddBook(request.form)
@@ -108,14 +108,12 @@ def add_Book():
         return render_template('admin_dashboard.html', form=form)
     return render_template('admin_dashboard.html', form=form)
 
-@app.route('/user_dashboard', methods=['GET','POST'])            
+@app.route('/user_dashboard', methods=['GET','POST'])
+@login_required
 def viewBooks():
-    """This function renders books database object to user_dashboard html template 
+    """This function renders books database object to user_dashboard html template
     and list all the books available in the Library"""
 
     if request.method=='GET':
         books = AvailableBooks.query.all()
         return render_template('user_dashboard.html', books=books)
-
-
-
