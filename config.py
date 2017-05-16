3ccfb45e50e4d339b28e78792888f5e7b863b540
Antoh1 <antotiro@gmail.com>
2017-05-16 16:29:32 +0300

    Changed sqlalchemy track modification to False

--- config.py
+++ config.py
@@ -5,11 +5,11 @@ import os
 DEBUG = True
 
 # Define the application directory
-basedir = os.path.abspath(os.path.dirname(__file__))  
+basedir = os.path.abspath(os.path.dirname(__file__))
 
-SQLALCHEMY_TRACK_MODIFICATIONS = True
+SQLALCHEMY_TRACK_MODIFICATIONS = False
 
-# Define the database - working with SQLite 
+# Define the database - working with SQLite
 SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db')
 SQLALCHEMY_MIGRATE_REPO = os.path.join(basedir, 'db_repository')
 
@@ -17,7 +17,7 @@ SQLALCHEMY_MIGRATE_REPO = os.path.join(basedir, 'db_repository')
 CSRF_ENABLED     = True
 
 # Use a secure, unique and absolutely secret key for
-# signing the data. 
+# signing the data.
 CSRF_SESSION_KEY = "willyouevereguessthis"
 
 # Secret key for signing cookies
